c34c2898df28b6925b4e2e0a9c6940bba81162b6
thunder775 <rahulmasih61@gmail.com>
2020-01-08 08:03:10 +0200

    refactored

--- bin/main.dart
+++ bin/main.dart
@@ -1,35 +1,37 @@
 /// day 60 remove first and last occurrence
 String clipString(String row) {
-  if(row.length==0) return '';
-  String first = row[0];
+  List<String> rowList = row.split('').toList();
+  if (row.length == 0) return '';
+  String first = rowList[0];
 
-  while (row.startsWith(first) || row.endsWith(first)) {
-    if (row.startsWith(first)) {
-      row = row.substring(1, row.length);
-    }
-    if (row.endsWith(first)) {
-      row = row.substring(0, row.length - 1);
-    }
+  while (rowList.isNotEmpty && rowList.first == first) {
+//    row = row.substring(1, row.length);
+    rowList.removeAt(0);
   }
-  return row;
+  while (rowList.isNotEmpty && rowList.last == first) {
+//    row = row.substring(0, row.length - 1);
+    rowList.removeLast();
+  }
+  return rowList.join();
 }
 
 int findLayers(List<String> matrix) {
-  matrix.forEach((string)=>print(string));
+//  matrix.forEach((string)=>print(string));
   int layerCount = 0;
-  while (matrix.any((string)=>string.isNotEmpty)) {
+  while (matrix.any((string) => string.isNotEmpty)) {
     for (int i = 0; i < matrix.length; i++) {
       matrix[i] = clipString(matrix[i]);
     }
     layerCount++;
-    matrix.forEach((string)=>print(string));
+//    matrix.forEach((string)=>print(string));
   }
-  return layerCount;
-}
+    return layerCount;
+  }
+
 
 void main() {
   print(clipString("AABCCBAA"));
-  
+
   print(findLayers([
     "AAAAAAAAAAA",
     "AABBBBBBBAA",
